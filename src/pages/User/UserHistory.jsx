c9ae24c831a6aea67a5b20d45d90f4e98ca6aa21
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2025-02-28 21:31:05 +0200

    add deletion of a specific order

--- src/pages/User/UserHistory.jsx
+++ src/pages/User/UserHistory.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './UserHistory.scss';
 import OrderCard from '../../components/OrderCard/OrderCard.jsx';
+import Btn from '../../components/Button/Btn.jsx';
+import { setOrders } from '../../store/slice/storeSlice.js';
 
 const UserHistory = () => {
     const { id } = useParams();
     const [order, setOrder] = useState(null);
     const userOrders = useSelector((state) => state.store.orders);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (id) {
             const searchOrder = userOrders.find((item) => item.id === id);
-            console.log(searchOrder);
             if (searchOrder) {
                 setOrder(searchOrder);
             }
@@ -23,6 +25,11 @@ const UserHistory = () => {
     if (!order) {
         return <div>Loading...</div>;
     }
+    const deleteOrder = () => {
+        const filterOrders = userOrders.filter((item) => item.id !== order.id);
+        dispatch(setOrders(filterOrders));
+        navigate(-1);
+    };
 
     return (
         <section className={'history'}>
@@ -30,6 +37,10 @@ const UserHistory = () => {
                 <div onClick={() => navigate(-1)} className="history_back">
                     Go back
                 </div>
+                <div className={'history_delete'}>
+                    <Btn text={'DELETE'} func={deleteOrder} />
+                </div>
+                <div className={'history_delete'}></div>
                 <div className="history_title">Order for: {order.date} </div>
                 <hr />
                 <div className="history_info">
